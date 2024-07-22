632eada35c250ca7530ced569d2b870a583cb4ed
RemyaQA <89893825+RemyaQA@users.noreply.github.com>
2024-07-22 10:39:05 +0530

    Initial commit

---
+++ src/Pkg_16072024/Lab001.java
@@ -0,0 +1,7 @@
+package Pkg_16072024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/Pkg_16072024/Lab106.java
@@ -0,0 +1,11 @@
+package Pkg_16072024;
+
+public class Lab106 {
+    public static void main(String[] args) {
+        int i = 34;
+        char n = "remya";
+        for(int i=1; i<5;i++)
+                   System.out.println(n);
+    }
+
+}
